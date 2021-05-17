b3ae44feebac3cb3138b3d8a9a40730935e96c3f
Ryan Erricson <ryanerricson@gmail.com>
2021-05-17 10:48:30 -0400

    commit tree bbff324638451e22a09c2d55b06b43a40a4480e0

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+
+class Polygon {
+
+    constructor(array){
+        this.array = array
+    }
+
+    get countSides(){
+        return this.array.length
+    }
+
+    get perimeter(){
+        return this.array.reduce((startingValue, accumulator) => {
+            return startingValue + accumulator
+        })
+    }
+}
+
+
+class Triangle extends Polygon {
+
+    get isValid() {
+        let side1 = this.array[0]
+        let side2 = this.array[1]
+        let side3 = this.array[2]
+        if (this.array.length === 3 && side1 + side2 > side3 && side1 + side3 > side2 && side3 + side2 > side1){
+            return true
+        } else {
+            return false
+        }
+        
+    }
+}
+
+    class Square extends Polygon {
+
+        get isValid(){
+            let side1 = this.array[0]
+            let side2 = this.array[1]
+            let side3 = this.array[2]
+            let side4 = this.array[3]
+
+            if (this.array.length === 4 && side1 == side2 && side1 == side3 && side1 == side4){
+                return true
+            } else {
+                return false
+            }
+        }
+
+        get area(){
+            let side1 = this.array[0]
+            let side2 = this.array[1]
+            if (this.isValid === true){
+                return side1 * side2
+            }
+        }
+    }
